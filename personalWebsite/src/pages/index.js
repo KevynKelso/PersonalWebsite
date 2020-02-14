bcf1a23308a3c6cc737dda6b6a5dd6cebaf1c043
Kevyn Kelso <kkelso@bscs.org>
2020-02-14 14:32:23 -0700

    add bootstrap dependency; begin refactoring main page

--- personalWebsite/src/pages/index.js
+++ personalWebsite/src/pages/index.js
@@ -1,48 +1,59 @@
 import React from "react"
 
-export default () => <div>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
- rel="stylesheet"
- integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-crossorigin="anonymous"></link>
+export default () => {
+  return (
+    <div>
+      <h1>Kevyn Kelso</h1>
+      <section>
+        <h2>Bio</h2>
+          <img 
+            alt="Kevyn Kelso"
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Bill_Gates_2014.jpg"
+          />
+        <p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke
+          fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip
+          four dollar toast. Shabby chic proident cronut prism. Affogato seitan
+          semiotics succulents organic viral deep v ut ipsum pok pok hella trust
+          fund lorem migas cred. Deserunt cillum direct trade locavore dolore
+          cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal
+          man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS
+          locavore etsy tempor.</p>
+      </section>
 
-<h1>Kevyn Kelso</h1>
+    <section>
+        <h2 class="text-primary">Projects</h2>
+            <h3>Programming</h3>
+            <img src="https://images.unsplash.com/photo-1580067532285-5575a5c8d36a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+            alt="a project"></img>
+            <p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard
+            salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby
+            chic proident cronut prism. Affogato seitan semiotics succulents organic viral
+            deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct
+            trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit
+            ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS
+            locavore etsy tempor.</p>
+            <a href="https://github.com/KevynKelso">Github</a>
+            <h3>Robotics</h3>
+            <p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard
+            salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby
+            chic proident cronut prism. Affogato seitan semiotics succulents organic viral
+            deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct
+            trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit
+            ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS
+            locavore etsy tempor.</p>
+    </section>
 
-<section>
-<h2>Bio</h2>
-<img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Bill_Gates_2014.jpg"
- alt="Kevyn Kelso"></img>
-<p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke
-  fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip
-  four dollar toast. Shabby chic proident cronut prism. Affogato seitan
-  semiotics succulents organic viral deep v ut ipsum pok pok hella trust
-  fund lorem migas cred. Deserunt cillum direct trade locavore dolore
-  cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal
-  man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS
-  locavore etsy tempor.</p>
-</section>
+    <section>
+    <h2 class="text-primary">Education</h2>
+    <img src="https://images.unsplash.com/photo-1578668189974-f7525881ae9f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+    alt="education building"></img>
+    <p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby chic proident cronut prism. Affogato seitan semiotics succulents organic viral deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS locavore etsy tempor.</p>
+    </section>
 
-<section>
-<h2 class="text-primary">Projects</h2>
-<h3>Programming</h3>
-<img src="https://images.unsplash.com/photo-1580067532285-5575a5c8d36a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-alt="a project"></img>
-<p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby chic proident cronut prism. Affogato seitan semiotics succulents organic viral deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS locavore etsy tempor.</p>
-<a href="https://github.com/KevynKelso">Github</a>
-<h3>Robotics</h3>
-<p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby chic proident cronut prism. Affogato seitan semiotics succulents organic viral deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS locavore etsy tempor.</p>
-</section>
+    <form>
+    <input type="text" placeholder="Email address"></input>
+    <button class="btn btn-primary btn-large">Submit</button>
+    </form>
 
-<section>
-<h2 class="text-primary">Education</h2>
-<img src="https://images.unsplash.com/photo-1578668189974-f7525881ae9f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-alt="education building"></img>
-<p>Lorem ipsum dolor amet voluptate gochujang drinking vinegar poke fam godard salvia cronut cardigan sunt hell of tofu vegan aliquip four dollar toast. Shabby chic proident cronut prism. Affogato seitan semiotics succulents organic viral deep v ut ipsum pok pok hella trust fund lorem migas cred. Deserunt cillum direct trade locavore dolore cupidatat consequat cardigan pug blue bottle prism id 8-bit ugh. Narwhal man bun iPhone tacos. Post-ironic kale chips butcher, gastropub VHS locavore etsy tempor.</p>
-</section>
-
-<form>
-<input type="text" placeholder="Email address"></input>
-<button class="btn btn-primary btn-large">Submit</button>
-</form>
-
-</div>
+  )
+}
